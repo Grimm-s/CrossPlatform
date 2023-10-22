61799f5a57e54b55a3a75cb9bac390122c90d695
semen.rybak <rsemen2002@gmail.com>
2023-10-23 01:08:26 +0300

    Initial commit

---
+++ CrossPlatformRybak/LAB1/Program.cs
@@ -0,0 +1,83 @@
+﻿class Program
+{
+    private const string InputFilePath = "..//..//..//input.txt";
+    private const string OutputFilePath = "..//..//..//output.txt";
+
+    static void Main()
+    {
+        if (!File.Exists(InputFilePath))
+        {
+            Console.WriteLine("File input.txt not found.");
+            return;
+        }
+
+        using var sr = new StreamReader(InputFilePath);
+        var n = ReadIntValue(sr);
+
+        if (n is < 1 or > 105)
+        {
+            Console.WriteLine("Error: n is out of range.");
+            return;
+        }
+
+        var a = ReadIntArray(sr);
+        var b = ReadIntArray(sr);
+
+        ValidateArrays(a, b, n);
+
+        var result = ProcessValues(a, b, n);
+
+        WriteOutput(result);
+    }
+
+    private static int ReadIntValue(StreamReader sr) => int.Parse(sr.ReadLine());
+
+    private static int[] ReadIntArray(StreamReader sr) => sr.ReadLine().Split().Select(int.Parse).ToArray();
+
+    private static void ValidateArrays(int[] a, int[] b, int n)
+    {
+        if (a.Any(ai => ai < 1) || b.Any(bi => bi > 109))
+        {
+            Console.WriteLine("Error: a or b values are out of range.");
+            Environment.Exit(0);
+        }
+
+        if (a.Length != n || b.Length != n)
+        {
+            Console.WriteLine("Error: the number of elements in a or b does not match the value of n.");
+            Environment.Exit(0);
+        }
+    }
+
+    private static string ProcessValues(int[] a, int[] b, int n)
+    {
+        var aSum = a.Sum();
+        var order = new List<int>();
+
+        var passed = false;
+        while (order.Count < n)
+        {
+            passed = false;
+            for (var i = 0; i < n; i++)
+            {
+                if (order.Contains(i + 1) || b[i] <= aSum - a[i]) continue;
+
+                order.Add(i + 1);
+                aSum -= a[i];
+                passed = true;
+                break;
+            }
+
+            if (!passed)
+                break;
+        }
+
+        return passed ? string.Join(" ", order) : "-1";
+    }
+
+    private static void WriteOutput(string result)
+    {
+        using var sw = new StreamWriter(OutputFilePath, false);
+        sw.WriteLine(result);
+    }
+}
---
+++ CrossPlatformRybak/LAB2/Program.cs
@@ -0,0 +1,74 @@
+﻿public class Program
+{
+    private const string InputFilePath = "..//..//..//input.txt";
+    private const string OutputFilePath = "..//..//..//output.txt";
+
+    public static void Main()
+    {
+        if (!File.Exists(InputFilePath))
+        {
+            throw new Exception("Error: File input.txt not found.");
+        }
+
+        var dirtySectors = LoadDirtySectorsFromFilePath(InputFilePath);
+        var minCleaningDistance = CalculateMinimumCleaningDistance(dirtySectors);
+        
+        WriteOutput(minCleaningDistance);
+    }
+
+    private static int CalculateDistanceBetweenSectors(Sector a, Sector b)
+    {
+        return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+    }
+
+    private static int CalculateMinimumCleaningDistance(List<Sector> dirtySectors)
+    {
+        var currentSector = new Sector { X = 1, Y = 1 };
+        var totalCleaningDistance = 0;
+
+        while (dirtySectors.Any())
+        {
+            var closestDirtySector = dirtySectors.OrderBy(s => s.Y)
+                                                 .ThenBy(s => CalculateDistanceBetweenSectors(currentSector, s))
+                                                 .First();
+            totalCleaningDistance += CalculateDistanceBetweenSectors(currentSector, closestDirtySector);
+            currentSector = closestDirtySector;
+            dirtySectors.Remove(closestDirtySector);
+        }
+
+        return totalCleaningDistance;
+    }
+
+    private static List<Sector> LoadDirtySectorsFromFilePath(string filePath)
+    {
+        var fileLines = File.ReadAllLines(filePath);
+        var numberOfSectors = int.Parse(fileLines[0]);
+
+        ValidateSectorCount(numberOfSectors, fileLines.Length - 1);
+
+        return fileLines.Skip(1)
+                        .Select(line => line.Split(' ')
+                                             .Select(int.Parse)
+                                             .ToArray())
+                        .Select(parts => new Sector { X = parts[0], Y = parts[1] })
+                        .ToList();
+    }
+
+    private static void ValidateSectorCount(int declaredCount, int actualCount)
+    {
+        if (declaredCount is < 1 or > 1000) throw new Exception("Error: n is out of range.");
+        if (declaredCount != actualCount) throw new Exception("Error: the number of elements does not match the value of n.");
+    }
+
+    private static void WriteOutput(int result)
+    {
+        using var sw = new StreamWriter(OutputFilePath, false);
+        sw.WriteLine(result);
+    }
+}
+
+public record Sector
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+}
