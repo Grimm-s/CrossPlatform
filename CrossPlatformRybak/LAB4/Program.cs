23f915b7b5ff09c5a4b5c8f4e97a65b08fd93aca
semen.rybak <rsemen2002@gmail.com>
2023-10-23 02:42:20 +0300

    Added part of lab 4

---
+++ CrossPlatformRybak/LAB4/Program.cs
@@ -0,0 +1,104 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using LAB4_LIB;
+
+namespace Lab4
+{
+    class Program
+    {
+        static int Main(string[] args)
+            => CommandLineApplication.Execute<ConsoleApp>(args);
+    }
+
+    [Command(Description = "Cross platform console util")]
+    [Subcommand(typeof(VersionCommand), typeof(RunCommand), typeof(SetPathCommand))]
+    class ConsoleApp
+    {
+        private void OnExecute(CommandLineApplication app)
+        {
+            Console.WriteLine("Use a command or --help for available options.");
+        }
+    }
+
+    [Command(Name = "version", Description = "Display version information")]
+    class VersionCommand
+    {
+        private void OnExecute()
+        {
+            Console.WriteLine("Author: Semen Rybak");
+            Console.WriteLine("Version: 1.0");
+        }
+    }
+
+    [Command(Name = "run", Description = "Run a specific lab")]
+    class RunCommand
+    {
+        [Option("-I|--input", Description = "Input file path")]
+        private string InputPath { get; } = null!;
+
+        [Option("-o|--output", Description = "Output file path")]
+        private string OutputPath { get; } = null!;
+
+        [Argument(0, Description = "The lab to run (e.g., lab1, lab2)")]
+        private string LabName { get; } = null!;
+
+        private void OnExecute()
+        {
+            if (string.IsNullOrWhiteSpace(LabName))
+            {
+                Console.WriteLine("Specify a lab to run (e.g., lab1).");
+                return;
+            }
+
+            if (LabName == "lab1")
+            {
+                Lab1.Run(Helpers.ResolvePath(InputPath, "input.txt"), Helpers.ResolvePath(OutputPath, "output.txt"));
+            }
+        }
+    }
+
+    [Command(Name = "set-path", Description = "Set the path for input and output files")]
+    class SetPathCommand
+    {
+        [Option("-p|--path", Description = "Path to the directory with input and output files")]
+        private string Path { get; } = null!;
+
+        private void OnExecute()
+        {
+            Environment.SetEnvironmentVariable("LAB_PATH", Path, EnvironmentVariableTarget.User);
+            Console.WriteLine($"LAB_PATH set to: {Path}");
+        }
+    }
+
+    static class Helpers
+    {
+        public static string ResolvePath(string providedPath, string defaultFileName)
+        {
+            if (!string.IsNullOrWhiteSpace(providedPath) && File.Exists(providedPath))
+            {
+                return providedPath;
+            }
+
+            var labPath = Environment.GetEnvironmentVariable("LAB_PATH",EnvironmentVariableTarget.User);
+
+            if (!string.IsNullOrWhiteSpace(labPath))
+            {
+                var envFilePath = Path.Combine(labPath, defaultFileName);
+
+                if (File.Exists(envFilePath))
+                {
+                    return envFilePath;
+                }
+            }
+            
+            var userHomeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            var defaultFilePath = Path.Combine(userHomeDirectory, defaultFileName);
+            
+            if (File.Exists(defaultFilePath))
+            {
+                return defaultFilePath;
+            }
+
+            throw new FileNotFoundException();
+        }
+    }
+}
---
+++ CrossPlatformRybak/LAB4_LIB/Lab1.cs
@@ -0,0 +1,83 @@
+﻿namespace LAB4_LIB;
+
+public static class Lab1
+{
+    public static void Run(string inputPath, string outputPath)
+    {
+
+        if (!File.Exists(inputPath))
+        {
+            Console.WriteLine($"File {inputPath} not found.");
+            return;
+        }
+
+        using var sr = new StreamReader(inputPath);
+        var n = ReadIntValue(sr);
+
+        if (n is < 1 or > 105)
+        {
+            Console.WriteLine("Error: n is out of range.");
+            return;
+        }
+
+        var a = ReadIntArray(sr);
+        var b = ReadIntArray(sr);
+
+        ValidateArrays(a, b, n);
+
+        var result = ProcessValues(a, b, n);
+
+        WriteOutput(outputPath,result);
+    }
+
+    private static int ReadIntValue(StreamReader sr) => int.Parse(sr.ReadLine());
+
+    private static int[] ReadIntArray(StreamReader sr) => sr.ReadLine().Split().Select(int.Parse).ToArray();
+
+    private static void ValidateArrays(int[] a, int[] b, int n)
+    {
+        if (a.Any(ai => ai < 1) || b.Any(bi => bi > 109))
+        {
+            Console.WriteLine("Error: a or b values are out of range.");
+            Environment.Exit(0);
+        }
+
+        if (a.Length != n || b.Length != n)
+        {
+            Console.WriteLine("Error: the number of elements in a or b does not match the value of n.");
+            Environment.Exit(0);
+        }
+    }
+
+    private static string ProcessValues(int[] a, int[] b, int n)
+    {
+        var aSum = a.Sum();
+        var order = new List<int>();
+
+        var passed = false;
+        while (order.Count < n)
+        {
+            passed = false;
+            for (var i = 0; i < n; i++)
+            {
+                if (order.Contains(i + 1) || b[i] <= aSum - a[i]) continue;
+
+                order.Add(i + 1);
+                aSum -= a[i];
+                passed = true;
+                break;
+            }
+
+            if (!passed)
+                break;
+        }
+
+        return passed ? string.Join(" ", order) : "-1";
+    }
+
+    private static void WriteOutput(string path, string result)
+    {
+        using var sw = new StreamWriter(path, false);
+        sw.WriteLine(result);
+    }
+}
