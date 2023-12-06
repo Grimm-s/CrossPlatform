3d6170ec0d004d8dd990346eca97239022b2b57d
semen.rybak <rsemen2002@gmail.com>
2023-12-06 02:51:08 +0200

    remove comments

--- CrossPlatformRybak/LAB5/Controllers/AccountController.cs
+++ CrossPlatformRybak/LAB5/Controllers/AccountController.cs
@@ -13,9 +13,6 @@ public class AccountController : Controller
     public async Task Login(string returnUrl = "/")
         {
             var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
-                // Indicate here where Auth0 should redirect the user after a login.
-                // Note that the resulting absolute Uri must be added to the
-                // **Allowed Callback URLs** settings for the app.
                 .WithRedirectUri(returnUrl)
                 .Build();
 
@@ -37,21 +34,13 @@ public class AccountController : Controller
         public async Task Logout()
         {
             var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
-                // Indicate here where Auth0 should redirect the user after a logout.
-                // Note that the resulting absolute Uri must be added to the
-                // **Allowed Logout URLs** settings for the app.
                 .WithRedirectUri(Url.Action("Index", "Home"))
                 .Build();
 
             await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         }
-
-        /// <summary>
-        /// This is just a helper action to enable you to easily see all claims related to a user. It helps when debugging your
-        /// application to see the in claims populated from the Auth0 ID Token
-        /// </summary>
-        /// <returns></returns>
+        
         [Authorize]
         public IActionResult Claims()
         {
